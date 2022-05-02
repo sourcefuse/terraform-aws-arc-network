676aefd6442a30999cf0100fc0af1e754c3aa6b6
chrisdeffer <chrisdeffer@gmail.com>
2022-05-02 18:30:49 -0400

    feature/REF-63 (#8)
    * feature/REF-63: example folder
    
    Co-authored-by: chrisdeffer <christopher.munson@sourcefuse.com>

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+
+module "ec2_bastion" {
+  source = "git::https://github.com/cloudposse/terraform-aws-ec2-bastion-server.git?ref=0.27.0"
+
+  enabled = module.this.enabled
+
+  instance_type               = var.instance_type
+  security_groups             = compact(concat([module.vpc.vpc_default_security_group_id], var.security_groups))
+  subnets                     = [for sub_id in module.public_subnets.az_subnet_ids : sub_id]
+  key_name                    = module.aws_key_pair.key_name
+  user_data                   = var.user_data
+  vpc_id                      = module.vpc.vpc_id
+  associate_public_ip_address = var.associate_public_ip_address
+
+  context = module.this.context
+}
+
 module "vpc" {
   source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=0.27.0"
   namespace  = var.namespace
@@ -53,23 +70,6 @@ module "aws_key_pair" {
   attributes          = ["ssh", "key"]
   ssh_public_key_path = var.ssh_key_path
   generate_ssh_key    = var.generate_ssh_key
-
-  context = module.this.context
-}
-
-
-module "ec2_bastion" {
-  source = "git::https://github.com/cloudposse/terraform-aws-ec2-bastion-server.git?ref=0.27.0"
-
-  enabled = module.this.enabled
-
-  instance_type               = var.instance_type
-  security_groups             = compact(concat([module.vpc.vpc_default_security_group_id], var.security_groups))
-  subnets                     = [for sub_id in module.public_subnets.az_subnet_ids : sub_id]
-  key_name                    = module.aws_key_pair.key_name
-  user_data                   = var.user_data
-  vpc_id                      = module.vpc.vpc_id
-  associate_public_ip_address = var.associate_public_ip_address
 
   context = module.this.context
 }
