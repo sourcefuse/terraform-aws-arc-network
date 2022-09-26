43ae8a8368bf0fcb2885e33588346db753c5107c
Travis Saucier <travis.saucier@sourcefuse.com>
2022-09-26 09:27:39 -0400

    adding additional outputs for network

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "public_subnet_ids" {
+  value = module.public_subnets.az_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.private_subnets.az_subnet_ids
+}
+
 output "public_subnet_cidrs" {
   value = module.public_subnets.az_subnet_cidr_blocks
 }
@@ -8,4 +16,8 @@ output "private_subnet_cidrs" {
 
 output "vpc_cidr" {
   value = module.vpc.vpc_cidr_block
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
