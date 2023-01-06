68d37867798caacb1774957b4a8b17393c67b1e4
James Crowley <james.crowley@sourcefuse.com>
2023-01-06 11:44:49 -0600

    remove invalid state information

--- example/state.tf
+++ example/state.tf
@@ -2,10 +2,5 @@ terraform {
   required_version = ">= 1.0, < 2.0"
 
   backend "s3" {
-    region         = "us-east-1"
-    key            = "terraform-aws-ref-arch-network/terraform.tfstate"
-    bucket         = "sf-ref-arch-terraform-state-dev"
-    dynamodb_table = "sf_ref_arch_terraform_state_dev"
-    encrypt        = true
   }
 }
