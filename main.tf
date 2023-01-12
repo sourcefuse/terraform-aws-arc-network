bb5231240acca2b0b83eb64a60def8443bffb143
Travis Saucier <travis.saucier@sourcefuse.com>
2023-01-11 22:17:28 -0500

    increment version. add tags to vpc_endpoints.

--- main.tf
+++ main.tf
@@ -54,6 +54,8 @@ module "vpc_endpoints" {
   vpc_id                  = module.vpc.vpc_id
   gateway_vpc_endpoints   = var.gateway_vpc_endpoints
   interface_vpc_endpoints = var.interface_vpc_endpoints
+
+  tags = var.tags
 }
 
 ################################################################################
