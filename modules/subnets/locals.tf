b95f4f492d68ceb912f2a9e1f8710b5833ea418d
Shubham Sinha <73998079+shubhamsinha-sf@users.noreply.github.com>
2024-02-10 18:01:21 +0530

    split locals for nat gateway ids from input and from the resource within the module (#74)

--- modules/subnets/locals.tf
+++ modules/subnets/locals.tf
@@ -7,5 +7,9 @@ locals {
   // then no routes to public internet are added.
   public_internet_mapping = { for x in var.private_subnets : x.name => replace(x.name, "-private-", "-public-") if var.nat_gateway_enabled == true }
 
-  subnet_ngw_ids = { for x in var.private_subnets : x.name => var.nat_gateway_enabled == true ? aws_nat_gateway.public[local.public_internet_mapping[x.name]].id : lookup(var.az_ngw_ids, x.availability_zone, null) }
+  subnet_ngw_ids_resource = { for x in var.private_subnets : x.name => var.nat_gateway_enabled == true ? aws_nat_gateway.public[local.public_internet_mapping[x.name]].id : null }
+
+  subnet_ngw_ids_custom = { for x in var.private_subnets : x.name => lookup(var.az_ngw_ids, x.availability_zone, null) }
+
+  subnet_ngw_ids = var.nat_gateway_enabled == true ? local.subnet_ngw_ids_resource : { for key,value in local.subnet_ngw_ids_custom : key => value if value != null }
 }
