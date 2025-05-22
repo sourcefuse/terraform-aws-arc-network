261332b5323e9d7ebce93aabac4cc4d092201463
chandra <Chandrashekar.reddy@sourcefuse.com>
2025-05-22 19:26:57 +0530

    update terratest go

--- examples/custom-subnets/local.tf
+++ examples/custom-subnets/local.tf
@@ -3,25 +3,59 @@ locals {
   prefix = "${var.namespace}-${var.environment}"
 
   subnet_map = {
-    "${local.prefix}-public" = {
-      name                    = "${local.prefix}-public"
-      cidr_block              = "10.0.0.0/18"
+    "${local.prefix}-public-az1" = {
+      name                    = "${local.prefix}-public-az1"
+      cidr_block              = "10.0.0.0/19"
       availability_zone       = "us-east-1a"
-      nat_gateway_name        = "${local.prefix}-ngtw01"
+      nat_gateway_name        = "${local.prefix}-az1-ngtw01"
       attach_nat_gateway      = false
       create_nat_gateway      = true
       attach_internet_gateway = true
-      map_public_ip_on_launch = true
     },
-    "${local.prefix}-private" = {
-      name                    = "${local.prefix}-private"
-      cidr_block              = "10.0.64.0/18"
+    "${local.prefix}-public-az2" = {
+      name                    = "${local.prefix}-public-az2"
+      cidr_block              = "10.0.32.0/19"
+      availability_zone       = "us-east-1b"
+      nat_gateway_name        = "${local.prefix}-az2-ngtw01"
+      attach_nat_gateway      = false
+      create_nat_gateway      = true
+      attach_internet_gateway = true
+    },
+    "${local.prefix}-db-az1" = {
+      name                    = "${local.prefix}-db-az1"
+      cidr_block              = "10.0.64.0/19"
       availability_zone       = "us-east-1a"
-      nat_gateway_name        = "${local.prefix}-ngtw01"
+      nat_gateway_name        = "${local.prefix}-az1-ngtw01"
       attach_nat_gateway      = true
       create_nat_gateway      = false
       attach_internet_gateway = false
-      map_public_ip_on_launch = false
+    },
+    "${local.prefix}-db-az2" = {
+      name                    = "${local.prefix}-db-az2"
+      cidr_block              = "10.0.96.0/19"
+      availability_zone       = "us-east-1b"
+      nat_gateway_name        = "${local.prefix}-az2-ngtw01"
+      attach_nat_gateway      = true
+      create_nat_gateway      = false
+      attach_internet_gateway = false
+    },
+    "${local.prefix}-app-az1" = {
+      name                    = "${local.prefix}-app-az1"
+      cidr_block              = "10.0.128.0/19"
+      availability_zone       = "us-east-1a"
+      nat_gateway_name        = "${local.prefix}-az1-ngtw01"
+      attach_nat_gateway      = true
+      create_nat_gateway      = false
+      attach_internet_gateway = false
+    },
+    "${local.prefix}-app-az2" = {
+      name                    = "${local.prefix}-app-az2"
+      cidr_block              = "10.0.160.0/19"
+      availability_zone       = "us-east-1b"
+      nat_gateway_name        = "${local.prefix}-az2-ngtw01"
+      attach_nat_gateway      = true
+      create_nat_gateway      = false
+      attach_internet_gateway = false
     }
   }
 
